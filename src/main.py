a2df2012304c3a29127f01a5afad8780f565c8d0
Roberto sousa <robertosousa186@gmail.com>
2019-12-09 22:02:39 -0200

    lista de exercicios python.org

---
+++ src/main.py
@@ -0,0 +1,6 @@
+print('Como testar se um número é positivo ou negativo? ')
+numero = int(input('Digite um número: '))
+if numero > 0:
+    print('o número', numero, 'é positivo !')
+else:
+    print(numero, 'é um número negativo!')
